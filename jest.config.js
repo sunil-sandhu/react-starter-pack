a217130cb45819cc1112e1243d8bda2a6cae1e15
Sunil Sandhu <sunil.sandhu87@gmail.com>
2020-09-26 17:50:38 +0200

    fixed regex typo in testMatch

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   transform: {
     "\\.(js|jsx)?$": "babel-jest",
   },
-  testMatch: ["<rootDir>/src/**(*.)test.{js, jsx}"], // finds test
+  testMatch: ["<rootDir>/src/**/(*.)test.{js, jsx}"], // finds test
   moduleFileExtensions: ["js", "jsx", "json", "node"],
   testPathIgnorePatterns: ["/node_modules/", "/public/"],
   setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
