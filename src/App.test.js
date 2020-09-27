37fe7f49c78c886531a44c2dcb0edc98452b0fab
Sunil Sandhu <sunil.sandhu87@gmail.com>
2020-09-27 14:08:08 +0200

    update to test

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { render } from "@testing-library/react";
-// import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 describe("<App/>", () => {
   test("renders React Starter Pack", () => {
-    const { getByText } = render(<App />);
-    expect(getByText(/react starter pack/i)).toBeInTheDocument();
+    render(<App />);
+    expect(screen.getByText(/react starter pack/i)).toBeInTheDocument();
   });
+
+  // Below is what our old test example looked like.
+  // Updated test following advice from...
+  // https://kentcdodds.com/blog/common-mistakes-with-react-testing-library
+
+  // test("renders React Starter Pack", () => {
+  //   const { getByText } = render(<App />);
+  //   expect(getByText(/react starter pack/i)).toBeInTheDocument();
+  // });
 });
